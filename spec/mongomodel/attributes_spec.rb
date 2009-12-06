9567a804f34be640fbbd62055917ef5538b572c1
Sam Pohlenz <sam@sampohlenz.com>
2009-12-07 09:46:42 +1030

    Attribute specs for Document and EmbeddedDocument

--- spec/mongomodel/attributes_spec.rb
+++ spec/mongomodel/attributes_spec.rb
@@ -2,44 +2,56 @@ require 'spec_helper'
 require 'active_support/core_ext/hash/indifferent_access'
 
 module MongoModel
-  describe Document do
-    AttributeTypes = {
-      String => "my string",
-      Integer => 99,
-      Float => 45.123,
-      Symbol => :foobar,
-      Boolean => false,
-      Array => [ 1, 2, 3, "hello", :world, [99, 100] ],
-      Hash => { :rabbit => 'hat', 'hello' => 12345 }.with_indifferent_access,
-      Date => lambda { Date.today },
-      Time => lambda { Time.now },
-      CustomClass => CustomClass.new('hello')
-    }
-    
+  AttributeTypes = {
+    String => "my string",
+    Integer => 99,
+    Float => 45.123,
+    Symbol => :foobar,
+    Boolean => false,
+    Array => [ 1, 2, 3, "hello", :world, [99, 100] ],
+    Hash => { :rabbit => 'hat', 'hello' => 12345 }.with_indifferent_access,
+    Date => Date.today,
+    Time => Time.now,
+    CustomClass => CustomClass.new('hello')
+  }
+  
+  specs_for(Document, EmbeddedDocument) do
     AttributeTypes.each do |type, value|
       describe "setting #{type} attributes" do
-        define_class(:TestDocument, Document) do
+        define_class(:TestDocument, described_class) do
           property :test_property, type
         end
-      
-        before(:each) do
-          @value = value.respond_to?(:call) ? value.call : value
+        
+        if specing?(EmbeddedDocument)
+          define_class(:ParentDocument, Document) do
+            property :child, TestDocument
+          end
+          
+          let(:parent) { ParentDocument.create!(:child => TestDocument.new(:test_property => value)) }
+          let(:child) { parent.child }
+          let(:reloaded) { ParentDocument.find(parent.id).child }
+          
+          subject { child }
+        else
+          subject { TestDocument.create!(:test_property => value) }
+          
+          let(:reloaded) { TestDocument.find(subject.id) }
         end
       
-        subject { TestDocument.create!(:test_property => @value) }
+        
       
         it "should read the correct value from attributes" do
-          subject.test_property.should == @value
+          subject.test_property.should == value
         end
       
         it "should read the correct value after reloading" do
-          TestDocument.find(subject.id).test_property.should == subject.test_property
+          reloaded.test_property.should == subject.test_property
         end
       end
     end
     
     describe "setting attributes with hash" do
-      define_class(:TestDocument, Document) do
+      define_class(:TestDocument, described_class) do
         property :test_property, String
         
         def test_property=(value)
