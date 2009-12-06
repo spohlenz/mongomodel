0a4aff63a64bdf1b9dca202323efb843df8d7bc6
Sam Pohlenz <sam@sampohlenz.com>
2009-12-06 12:25:17 +1030

    Fix Time equality for Ruby 1.8

--- spec/support/time.rb
+++ spec/support/time.rb
@@ -1,6 +1,6 @@
 # For the purposes of tests, we ignore fractions of a second when comparing Time objects
 class Time
   def ==(other)
-    super(other) || to_s == other.to_s
+    super(other) || utc.to_s == other.utc.to_s if other
   end
 end
