c29f14ea31cdc0418805ec7d1bba5ce118310f8c
Sam Pohlenz <sam@sampohlenz.com>
2010-04-06 10:04:06 +0930

    Require singleton_class from kernel

--- lib/mongomodel/document/scopes.rb
+++ lib/mongomodel/document/scopes.rb
@@ -1,6 +1,6 @@
 require 'active_support/core_ext/module/aliasing'
 require 'active_support/core_ext/module/delegation'
-require 'active_support/core_ext/object/singleton_class'
+require 'active_support/core_ext/kernel/singleton_class'
 
 module MongoModel
   module DocumentExtensions
