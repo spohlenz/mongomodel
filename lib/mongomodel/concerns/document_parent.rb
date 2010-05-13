7f32da372ac0a20e9b35adf0b3013b6bbd0ea72e
Sam Pohlenz <sam@sampohlenz.com>
2010-05-13 15:03:52 +0930

    Fix parent_document proc on Ruby 1.9

--- lib/mongomodel/concerns/document_parent.rb
+++ lib/mongomodel/concerns/document_parent.rb
@@ -3,7 +3,14 @@ module MongoModel
     extend ActiveSupport::Concern
     
     def parent_document
-      @_parent_document.is_a?(Proc) ? @_parent_document.call(self) : @_parent_document
+      if @_parent_document.respond_to?(:call)
+        case @_parent_document.arity
+        when 0 then @_parent_document.call
+        else        @_parent_document.call(self)
+        end
+      else
+        @_parent_document
+      end
     end
     
     def parent_document=(doc)
