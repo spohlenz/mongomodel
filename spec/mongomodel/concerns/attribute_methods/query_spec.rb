b9861df031b39fefc093b1a988001d95f4cb50d5
Sam Pohlenz <sam@sampohlenz.com>
2010-05-28 14:20:31 +0930

    Added specs for query_attribute involving boolean attribute

--- spec/mongomodel/concerns/attribute_methods/query_spec.rb
+++ spec/mongomodel/concerns/attribute_methods/query_spec.rb
@@ -4,23 +4,50 @@ module MongoModel
   specs_for(Document, EmbeddedDocument) do
     define_class(:TestDocument, described_class) do
       property :foo, String
+      property :boolean, Boolean
     end
     
     subject { TestDocument.new }
     
     describe "#query_attribute" do
-      it "should return true if the attribute is not blank" do
-        subject.foo = 'set foo'
-        subject.query_attribute(:foo).should be_true
+      context "string attribute" do
+        it "should return true if the attribute is not blank" do
+          subject.foo = 'set foo'
+          subject.query_attribute(:foo).should be_true
+          subject.foo?.should be_true
+        end
+      
+        it "should return false if the attribute is nil" do
+          subject.foo = nil
+          subject.query_attribute(:foo).should be_false
+          subject.foo?.should be_false
+        end
+      
+        it "should return false if the attribute is blank" do
+          subject.foo = ''
+          subject.query_attribute(:foo).should be_false
+          subject.foo?.should be_false
+        end
       end
       
-      it "should return false if the attribute is blank" do
-        subject.foo = ''
-        subject.query_attribute(:foo).should be_false
-      end
+      context "boolean attribute" do
+        it "should return true if the attribute is true" do
+          subject.boolean = true
+          subject.query_attribute(:boolean).should be_true
+          subject.boolean?.should be_true
+        end
+        
+        it "should return false if the attribute is nil" do
+          subject.boolean = nil
+          subject.query_attribute(:boolean).should be_false
+          subject.boolean?.should be_false
+        end
       
-      it "should create a query method" do
-        subject.foo?.should == subject.query_attribute(:foo)
+        it "should return false if the attribute is false" do
+          subject.boolean = false
+          subject.query_attribute(:boolean).should be_false
+          subject.boolean?.should be_false
+        end
       end
     end
   end
