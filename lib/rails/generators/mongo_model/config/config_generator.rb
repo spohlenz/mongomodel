af561c73c6b75b51a4bf772dff9c04b608e03cdf
Kevin <kevin.melchert@gmail.com>
2010-07-30 21:49:16 +0200

    cleanup

--- lib/rails/generators/mongo_model/config/config_generator.rb
+++ lib/rails/generators/mongo_model/config/config_generator.rb
@@ -1,10 +1,8 @@
-# require 'thor-ext'
 require 'rails/generators/base'
 
 module MongoModel
   module Generators
     class ConfigGenerator < Rails::Generators::Base
-      # include ThorExtensions
       source_root File.expand_path('../templates', __FILE__)
 
       argument :database, :type => :string, :default => 'mongo_db_default'
@@ -13,15 +11,10 @@ module MongoModel
         template "mongomodel.yml" , "config/mongomodel.yml"
       end
 
-      # def self.source_root
-      #   @source_root ||= File.expand_path('../templates', __FILE__)
-      # end
-
       def self.banner
-        "#{$0} mongomodel:#{generator_name} #{self.arguments.map{ |a| a.usage }.join(' ')} [options]"
+        "rails generate mongo_model:#{generator_name} #{self.arguments.map{ |a| a.usage }.join(' ')} [options]"
       end
 
-      
     end
   end
 end
