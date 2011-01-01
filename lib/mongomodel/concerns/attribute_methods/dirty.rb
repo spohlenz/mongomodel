616e01c6ded0560630a9eee2fb85d78af5705c03
Kevin <kevin.melchert@gmail.com>
2011-01-01 16:25:45 +0100

    use hash with indifferent access in original_attributes

--- lib/mongomodel/concerns/attribute_methods/dirty.rb
+++ lib/mongomodel/concerns/attribute_methods/dirty.rb
@@ -12,7 +12,7 @@ module MongoModel
       
       # Returns the attributes as they were before any changes were made to the document.
       def original_attributes
-        {}.merge(attributes).merge(changed_attributes)
+        {}.with_indifferent_access.merge(attributes).merge(changed_attributes)
       end
     
     protected
