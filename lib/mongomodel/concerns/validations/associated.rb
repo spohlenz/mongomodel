07fa9f34136bfc3318a31b2484bf0e666b32cbe8
Sam Pohlenz <sam@sampohlenz.com>
2011-10-18 15:10:55 +1030

    Ensure valid? is called on all associated records when validating

--- lib/mongomodel/concerns/validations/associated.rb
+++ lib/mongomodel/concerns/validations/associated.rb
@@ -2,7 +2,7 @@ module MongoModel
   module Validations
     class AssociatedValidator < ActiveModel::EachValidator
       def validate_each(record, attribute, value)
-        return if Array(value).all? { |r| r.nil? || r.valid?(record.validation_context) }
+        return if Array(value).map { |r| r.nil? || r.valid?(record.validation_context) }.all?
         record.errors.add(attribute, :invalid, options.merge(:value => value))
       end
     end
