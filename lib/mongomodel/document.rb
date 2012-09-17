077c4e2c5eca7cc5b1c64cdd0a8e9be003d07348
Sam Pohlenz <sam@sampohlenz.com>
2012-09-17 14:25:37 +0930

    Use class_attribute instead of cattr_accessor for per_page attribute

--- lib/mongomodel/document.rb
+++ lib/mongomodel/document.rb
@@ -1,4 +1,4 @@
-require 'active_support/core_ext/class/attribute_accessors'
+require 'active_support/core_ext/class/attribute'
 require 'active_support/core_ext/hash/reverse_merge'
 require 'active_support/core_ext/hash/deep_merge'
 require 'active_support/core_ext/string/inflections'
@@ -22,7 +22,7 @@ module MongoModel
     
     self.abstract_class = true
     
-    cattr_accessor :per_page
+    class_attribute :per_page
     self.per_page = 20
   end
 end
